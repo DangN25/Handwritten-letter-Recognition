24a9b822465bcb186418b3cca49553588aef5481
DangN25 <dang.nguyen0625@gmail.com>
2024-06-19 19:17:31 -0500

    Update
    finished but need improvement

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Handwritten-letter-Recognition
 second neuro network project
+
+(Note: VERY BAD ACCURACY NEED IMPROVEMENT)
+
+Require package
+-numpy
+-tensorflow
+-cv2
+-matplotlib
+
+Dataset citation:
+Cohen, G., Afshar, S., Tapson, J., & van Schaik, A. (2017). EMNIST: an extension of MNIST to handwritten letters. Retrieved from http://arxiv.org/abs/1702.05373
--- main.py
+++ main.py
@@ -1,1 +1,62 @@
- 
+#===================================IMPORT===================================#
+import os 
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf 
+import tensorflow_datasets as tfds
+
+
+#===================================TRAINNING/TESTING-DATA===================================#
+(train_ds, test_ds), ds_info = tfds.load('emnist/letters', 
+    split=['train', 'test'], 
+    shuffle_files=True, 
+    as_supervised=True, 
+    with_info=True
+)
+
+#===================================PREPROCESS-DATA===================================#
+def preprocess_img(image, label):
+    #Convert images from `uint8` -> `float32`.
+    return tf.cast(image, tf.float32) / 255.0, label
+
+#apply preprocess_img to both train and test dataset
+train_ds = train_ds.map(preprocess_img)
+train_ds = train_ds.batch(32)
+test_ds = test_ds.map(preprocess_img)
+test_ds = test_ds.batch(32)
+
+#===================================MODEL===================================#
+model = tf.keras.models.Sequential()
+
+#===================================LAYERS-SETUP===================================#
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(27, activation='softmax'))
+
+#===================================COMPILE===================================#
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_ds, epochs=10, validation_data=test_ds)
+
+model.save('handwritten.model.keras')
+
+#===================================LOAD-MODEL===================================#
+model= tf.keras.models.load_model("handwritten.model.keras")
+
+
+#===================================PREDICT===================================#
+im_num = 1
+while os.path.isfile(f"Letters/letter{im_num}.png"):
+    try:
+        img = cv2.imread(f"Letters/letter{im_num}.png")[:,:,0]
+        img = np.invert(np.array([img]))
+        prediction= model.predict(img)
+        print(f"This letter is {chr(ord('@')+np.argmax(prediction))}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("Error!")
+    finally:
+        im_num+=1
